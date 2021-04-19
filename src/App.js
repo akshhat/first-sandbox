4f5af179cac3ad0f68d152b80a053cce5eabbad6
Akshhat Srivastava <42445365+akshhat@users.noreply.github.com>
2021-04-19 21:41:10 +0530

    test-commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./styles.css";
 
 export default function App() {
-  return <div className="App">Hello World by Akshhat Srivastava</div>;
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
 }
